0098b15edbca3262197040a42d1fd23de0403284
cynthia <cchelangat9892gmail.com>
2022-09-21 13:56:23 +0300

    commit tree 0f52e74e4caadf18a77abbafb5bd72d9019a8e74

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About () {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
@@ -25,8 +32,11 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
 
 export default App;
+
